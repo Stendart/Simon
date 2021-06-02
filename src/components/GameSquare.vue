91de8dd1ecccbb379b3a0487499f35c064a75362
Stendart <Reiden1998@yandex.ua>
2021-06-02 23:10:18 +0300

    add sound

---
+++ src/components/GameSquare.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="square" :class="{'square-active' : rotation === 0}"
+         :style="{backgroundColor: color, transform: 'rotate(' + rotation + 'deg)'}"
+         @click="clickHandler"
+    >
+        <audio ref="audio" :src="soundUrl"></audio>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "GameSquare",
+    props: {
+      color: String,
+      sound: String,
+      rotation: {
+        type: Number,
+        default: 0
+      }
+    },
+    data() {
+      return {
+        test: ''
+      }
+    },
+    methods: {
+      clickHandler() {
+        console.log(this.$refs.audio)
+        this.$refs.audio.play()
+
+      }
+    },
+    computed: {
+      soundUrl () {
+        console.log('hana')
+        return require('../assets/sound/'+this.sound+'.mp3')//require(this.sound)
+        // The path could be '../assets/img.png', etc., which depends on where your vue file is
+      }
+    },
+    // mounted() {
+    //   console.log('sound =', this.sound)
+    //   this.test = require(this.sound) //'../assets/sound/1.mp3'
+    // }
+  }
+</script>
+
+<style scoped lang="scss">
+.square {
+    width: 200px;
+    height: 200px;
+    border: 2px solid white;
+    border-radius: 50% 0 0 0;
+
+    opacity: .8;
+
+    &:hover {
+        border: 2px solid black;
+        opacity: 1;
+    }
+}
+
+.square-active {
+    opacity: 1;
+}
+
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <GameSquare v-for="square in squaresConfig"
+                :color="square.color"
+                :key="square.color"
+                :rotation="square.rotate"
+                :sound="square.sound">
+    </GameSquare>
+  </div>
+</template>
+
+<script>
+import GameSquare from '../components/GameSquare';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      squaresConfig: [
+        {
+          color: '#FF5643',
+          rotate: 0,
+          sound: '1' //'../assets/sound/1.mp3'
+        },
+        {
+          color: '#1e90ff',
+          rotate: 90,
+          sound: '2'
+        },
+        {
+          color: '#FEEF33',
+          rotate: 270,
+          sound: '3'
+        },
+        {
+          color: '#BEDE15',
+          rotate: 180,
+          sound: '4'
+        },
+      ],
+      colors: ['#FF5643', '#1e90ff', '#FEEF33', '#BEDE15'],
+    }
+  },
+  components: {
+    GameSquare
+  }
+}
+</script>
+<style>
+  .home {
+    max-width: 400px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+</style>
